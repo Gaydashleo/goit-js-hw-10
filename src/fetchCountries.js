d8d7d6ed3125651562836c95ebdfd3b040a3280a
Leonid Haidash <Gaydashleo@gmail.com>
2022-08-02 13:28:21 +0300

    2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,7 @@
-fetchCountries('name')
+const inputFormEl = document.querySelector("#search-box");
+const countryListEl = document.querySelector(".country-list");
+
+fetchCountries('https://restcountries.com/v3.1/name/{name}')
   .then((respose) => respose.json())
   .then((data) => {
     console.log(data);
